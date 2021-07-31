d1d5a51be340d0de00182ffc2322f02ac77ee379
parkeunsik <qkrdmstlr3@naver.com>
2021-07-31 21:45:40 +0900

    [feat] [#2] emotion JS 설정
    - css in JS라이브러리인 emotionJS 초기 설정

---
+++ src/common/theme.ts
@@ -0,0 +1,16 @@
+const theme = {
+  color: {
+    color_brightness_000: '#000000',
+    color_brightness_100: '#323232',
+    color_brightness_200: '#464646',
+    color_brightness_300: '#5a5a5a',
+    color_brightness_400: '#6e6e6e',
+    color_brightness_500: '#828282',
+    color_brightness_600: '#969696',
+    color_brightness_700: '#aaaaaa',
+    color_brightness_800: '#dcdcdc',
+    color_brightness_900: '#ffffff',
+  },
+};
+
+export default theme;
