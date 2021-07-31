f8b2a0cbfce82bba7e29bb83e9da1ea535dddd1c
parkeunsik <qkrdmstlr3@naver.com>
2021-08-01 01:33:27 +0900

    [chore] [#2] storybook 설정 추가
    - 스토리북에 적용될 global style을 추가하였다
    - addon추가

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,23 @@
+import GlobalStyle from '../src/common/GlobalStyle';
+import React from 'react';
+import { addDecorator } from '@storybook/react';
+import { MemoryRouter } from 'react-router';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
+
+export const decorators = [
+  (Story) => (
+    <MemoryRouter initialEntries={['/']}>
+      <Story />
+      <GlobalStyle />
+    </MemoryRouter>
+  ),
+];
