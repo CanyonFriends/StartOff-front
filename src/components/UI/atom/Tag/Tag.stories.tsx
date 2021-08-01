1f572d95fd26b671d54615b4bf5f54303e6939a1
parkeunsik <qkrdmstlr3@naver.com>
2021-08-01 19:05:45 +0900

    [feat] [#4] Tag Atom 컴포넌트 생성
    - 태그 역할을 하는 atom컴포넌트를 만들었다
      - 스토리북 파일 작성
      - 스타일 파일 작성

---
+++ src/components/UI/atom/Tag/Tag.stories.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { withKnobs, text, color } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
+import Tag from '.';
+import StyleWrapper from '../../../../common/styleWrapper';
+import theme from '../../../../common/theme';
+
+export default {
+  title: 'Atom/Tag',
+  component: Tag,
+  decorators: [withKnobs],
+};
+
+const onClickAction = action('click');
+
+export const unavailableCloseTag = (): React.ReactElement => {
+  const label = text('TAG', 'typescript');
+  const backgroundColor = color('BACKGROUND COLOR', theme.color.color_primary_300);
+
+  return (
+    <StyleWrapper>
+      <div>
+        <div className="description">no color</div>
+        <Tag>{label}</Tag>
+      </div>
+      <div>
+        <div className="description">primary color</div>
+        <Tag backgroundColor={backgroundColor}>{label}</Tag>
+      </div>
+    </StyleWrapper>
+  );
+};
+
+export const availableCloseTag = (): React.ReactElement => {
+  const label = text('TAG', 'typescript');
+  const backgroundColor = color('BACKGROUND COLOR', theme.color.color_primary_300);
+
+  return (
+    <StyleWrapper>
+      <div>
+        <div className="description">no color</div>
+        <Tag onClickClose={onClickAction}>{label}</Tag>
+      </div>
+      <div>
+        <div className="description">primary color</div>
+        <Tag onClickClose={onClickAction} backgroundColor={backgroundColor}>
+          {label}
+        </Tag>
+      </div>
+    </StyleWrapper>
+  );
+};
