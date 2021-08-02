93dd749d9098e7713c1084b4dcf7d38c5a8e9cbf
parkeunsik <qkrdmstlr3@naver.com>
2021-08-03 01:05:22 +0900

    [feat] [#4] Input Atom 컴포넌트 생성
    - Input 역할을 하는 atom컴포넌트를 만들었다
      - 스토리북 파일 작성
      - 스타일 파일 작성

---
+++ src/components/UI/atom/Input/Input.stories.tsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { withKnobs, text as textKnob } from '@storybook/addon-knobs';
+import Input from '.';
+import Label from '../Label';
+import StyleWrapper from '../../../../common/styleWrapper';
+
+export default {
+  title: 'Atom/Input',
+  component: Input,
+  decorators: [withKnobs],
+};
+
+export const inputType = (): React.ReactElement => {
+  const placeholderText = '';
+  const [text, setText] = useState('text');
+  const [email, setEmail] = useState('email');
+  const [password, setPassword] = useState('password');
+
+  return (
+    <StyleWrapper>
+      <div>
+        <div className="description">placeholder</div>
+        <Input type="text" value={placeholderText} onChange={() => {}} placeholder="placeholder" />
+      </div>
+      <div>
+        <div className="description">text</div>
+        <Input type="text" value={text} onChange={(event) => setText(event.target.value)} />
+      </div>
+      <div>
+        <div className="description">email</div>
+        <Input type="email" value={email} onChange={(event) => setEmail(event.target.value)} />
+      </div>
+      <div>
+        <div className="description">password</div>
+        <Input type="password" value={password} onChange={(event) => setPassword(event.target.value)} />
+      </div>
+    </StyleWrapper>
+  );
+};
+
+export const inputWithLabel = (): React.ReactElement => {
+  const [text, setText] = useState('text');
+  const label = textKnob('LABEL', 'Label');
+  const groupName = 'group';
+
+  return (
+    <div>
+      <Label group={groupName}>{label}</Label>
+      <Input value={text} id={groupName} onChange={(event) => setText(event.target.value)} />
+    </div>
+  );
+};
--- src/components/UI/index.ts
+++ src/components/UI/index.ts
@@ -1,5 +1,6 @@
 /** Atom */
 export { default as Button } from './atom/Button';
 export { default as Icon } from './atom/Icon';
+export { default as Input } from './atom/Input';
 export { default as Label } from './atom/Label';
 export { default as Tag } from './atom/Tag';
