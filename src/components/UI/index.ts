a9f441a69cbb039aab16803b4d1a6bfde9243991
parkeunsik <qkrdmstlr3@naver.com>
2021-08-03 21:54:30 +0900

    [refactor] [#4] UI/index.ts파일 이동
    - 각 atomic폴더마다 index파일을 두는 것이 후에 import하는데 명시적이라고 생각

--- src/components/UI/index.ts
+++
@@ -1,9 +0,0 @@
-/** Atom */
-export { default as Anchor } from './atom/Anchor';
-export { default as Button } from './atom/Button';
-export { default as Icon } from './atom/Icon';
-export { default as Input } from './atom/Input';
-export { default as Label } from './atom/Label';
-export { default as Tag } from './atom/Tag';
-export { default as Image } from './atom/Image';
-export { default as Title } from './atom/Title';
