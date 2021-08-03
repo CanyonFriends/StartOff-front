12d28fabb0b4ab95fbf12070627f85e2ef9aec0d
yoo chang heon <dbckdgjs1130@naver.com>
2021-08-03 15:57:42 +0900

    리뷰바탕으로 수정

--- src/components/UI/atom/Image/styled.ts
+++ src/components/UI/atom/Image/styled.ts
@@ -1,13 +1,11 @@
 import styled from '@emotion/styled';
 
 interface ImageStyleProps {
-  src: string;
   width: string;
   height: string;
 }
 
 export const Image = styled.img<ImageStyleProps>`
-  src: url(${(props) => props.src});
   height: ${(props) => props.height};
   width: ${(props) => props.width};
 `;
