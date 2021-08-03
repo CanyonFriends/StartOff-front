2289f819023b667ce7f7fd1aacc54629520cc459
yoo chang heon <dbckdgjs1130@naver.com>
2021-08-04 05:11:01 +0900

    [feat][#4] Title 컴포넌트 생성

--- src/components/UI/@types/index.ts
+++ src/components/UI/@types/index.ts
@@ -9,9 +9,13 @@ export type LabelSizeType = 'small' | 'medium' | 'large' | 'extraLarge';
 
 export type InputType = 'text' | 'email' | 'password';
 
+<<<<<<< HEAD
 export type AnchorSizeType = 'small' | 'medium' | 'large' | 'extraLarge';
 
 export type TitleSizeType = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
 
 /** Molecule */
 export type SortDirectionType = 'column' | 'row';
+=======
+export type TitleSizeType = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
+>>>>>>> 4fcf06e ([feat][#4] Title 컴포넌트 생성)
---
+++ src/components/UI/index.ts~4fcf06e ([feat][#4] Title 컴포넌트 생성)
@@ -0,0 +1,8 @@
+/** Atom */
+export { default as Button } from './atom/Button';
+export { default as Icon } from './atom/Icon';
+export { default as Input } from './atom/Input';
+export { default as Label } from './atom/Label';
+export { default as Tag } from './atom/Tag';
+export { default as Image } from './atom/Image';
+export { default as Title } from './atom/Title';
