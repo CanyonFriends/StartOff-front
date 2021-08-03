f0a404ef1af62f627873c58ba62fdd768f9d812c
yoo chang heon <dbckdgjs1130@naver.com>
2021-08-03 15:57:42 +0900

    리뷰바탕으로 수정

--- src/components/UI/atom/Image/Image.stories.tsx
+++ src/components/UI/atom/Image/Image.stories.tsx
@@ -9,15 +9,41 @@ export default {
   decorators: [withKnobs],
 };
 
-export const image100x100 = (): React.ReactElement => {
+export const imageSizeByPercent = (): React.ReactElement => {
   const imagePath: string = 'http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg';
-  const h: string = '100%';
-  const w: string = '100%';
   return (
     <StyleWrapper>
       <div>
-        <div className="description">100*100</div>
-        <Image width={h} height={w} src={imagePath} alt="hello" />
+        <div className="description">25% x 25%</div>
+        <Image width="25%" height="25%" src={imagePath} alt="hello" />
+      </div>
+      <div>
+        <div className="description">50% x 50%</div>
+        <Image width="50%" height="50%" src={imagePath} alt="hello" />
+      </div>
+      <div>
+        <div className="description">100% x 100%</div>
+        <Image width="100%" height="100%" src={imagePath} alt="hello" />
+      </div>
+    </StyleWrapper>
+  );
+};
+
+export const imageSizeByPixel = (): React.ReactElement => {
+  const imagePath: string = 'http://t1.daumcdn.net/friends/prod/editor/dc8b3d02-a15a-4afa-a88b-989cf2a50476.jpg';
+  return (
+    <StyleWrapper>
+      <div>
+        <div className="description">25px x 25px</div>
+        <Image width="25px" height="25px" src={imagePath} alt="hello" />
+      </div>
+      <div>
+        <div className="description">50px x 50px</div>
+        <Image width="50px" height="50px" src={imagePath} alt="hello" />
+      </div>
+      <div>
+        <div className="description">100px x 100px</div>
+        <Image width="100px" height="100px" src={imagePath} alt="hello" />
       </div>
     </StyleWrapper>
   );
