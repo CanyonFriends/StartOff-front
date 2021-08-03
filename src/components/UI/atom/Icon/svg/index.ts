5f3908b61d8c267e592109aacc14ebd2ba792a16
parkeunsik <qkrdmstlr3@naver.com>
2021-08-03 21:53:36 +0900

    [feat] [#4] Icon추가
    - 로그인 로그아웃 로고 아이콘 추가

--- src/components/UI/atom/Icon/svg/index.ts
+++ src/components/UI/atom/Icon/svg/index.ts
@@ -1,1 +1,4 @@
 export { default as leftChevron } from './LeftChevron';
+export { default as Login } from './Login';
+export { default as Logout } from './Logout';
+export { default as Logo } from './Logo';
