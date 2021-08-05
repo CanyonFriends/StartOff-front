4a10456a227ce260d284ad4804811947ffc0ee8f
yoo chang heon <dbckdgjs1130@naver.com>
2021-08-05 15:36:12 +0900

    [feat][#4] TexArea 컴포넌트 생성

---
+++ src/components/UI/atom/TextArea/TextArea.stories.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import { withKnobs } from '@storybook/addon-knobs';
+import TextArea from '.';
+import StyleWrapper from '../../../../common/styleWrapper';
+
+export default {
+  title: 'Atom/TextArea',
+  component: TextArea,
+  decorators: [withKnobs],
+};
+
+export const TextAreawithPlaceholder = (): React.ReactElement => {
+  const [text, setText] = useState('text');
+  return (
+    <StyleWrapper>
+      <div>
+        <div className="description">Textbox 10 x 30</div>
+        <TextArea
+          rows={10}
+          cols={30}
+          placeholder="placeholder"
+          value={text}
+          onChange={(event) => setText(event.target.value)}
+        />
+      </div>
+      <div>
+        <div className="description">Textbox 5 x 10</div>
+        <TextArea
+          rows={5}
+          cols={10}
+          placeholder="placeholder"
+          value={text}
+          onChange={(event) => setText(event.target.value)}
+        />
+      </div>
+    </StyleWrapper>
+  );
+};
---
+++ src/components/UI/atom/TextArea/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import * as Style from './styled';
+
+interface TextAreaProps {
+  value: string;
+  id?: string;
+  placeholder?: string;
+  name?: string;
+  cols?: number;
+  rows?: number;
+  onChange: (event: React.ChangeEvent<HTMLTextAreaElement>) => void;
+}
+
+function TextArea({ id = '', placeholder = '', name = '', cols = 20, rows = 20, ...props }: TextAreaProps) {
+  return <Style.TextArea id={id} name={name} placeholder={placeholder} cols={cols} rows={rows} {...props} />;
+}
+
+export default TextArea;
