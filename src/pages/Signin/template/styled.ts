80eeaf36faab748e127e0830b16a5121e6186f6b
parkeunsik <qkrdmstlr3@naver.com>
2021-08-08 00:02:33 +0900

    [feat] [#16] signin page template구현
    - signin page의 템플릿을 구현
    - atom molecule organism을 합쳐서 하나의 템플릿을 구현하였다

---
+++ src/pages/Signin/template/styled.ts
@@ -0,0 +1,26 @@
+import styled from '@emotion/styled';
+import theme from '../../../common/theme';
+
+export const Container = styled.main`
+  position: absolute;
+  top: 45%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+export const MoveContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 10px;
+  margin-bottom: 30px;
+`;
+
+export const NoAccountText = styled.span`
+  font-size: 1.4rem;
+  color: ${theme.color.color_brightness_500};
+`;
+
+export const GithubSigninText = styled.span`
+  margin-left: 3rem;
+`;
