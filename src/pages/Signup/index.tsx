e5b9e194f8cb1a8a97ca04e2894be4953c65c9df
parkeunsik <qkrdmstlr3@naver.com>
2021-08-10 01:07:47 +0900

    [feat] [#23] Signup apge 라우팅 설정
    - 라우팅 경로 등록
    - 경로에 따른 컴포넌트 로드

---
+++ src/pages/Signup/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import SignupTemplate from './template';
+
+function Signup() {
+  return <SignupTemplate />;
+}
+
+export default Signup;
---
+++ src/pages/Signup/template/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SignupTemplate() {
+  return <div>signup</div>;
+}
+
+export default SignupTemplate;
