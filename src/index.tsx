c0aa44e286a76a5cda21f7c6b23aee32d46a1ae9
parkeunsik <qkrdmstlr3@naver.com>
2021-08-10 21:49:20 +0900

    [chore] 통신을 위한 axios설치
    - 서버와 restapi통신을 위해 axios설치

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import axios from 'axios';
 
 import App from './App';
 import GlobalStyle from './common/GlobalStyle';
+import { SERVER_URL } from './config';
+
+axios.defaults.baseURL = `${SERVER_URL}/api`;
 
 ReactDOM.render(
   <BrowserRouter>
