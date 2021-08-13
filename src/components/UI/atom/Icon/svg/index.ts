cbc6e38aa4eaf2321c370bac61c01d2508fe1db2
parkeunsik <qkrdmstlr3@naver.com>
2021-08-14 01:49:19 +0900

    [feat] 체크 / 경고 아이콘 추가

--- src/components/UI/atom/Icon/svg/index.ts
+++ src/components/UI/atom/Icon/svg/index.ts
@@ -2,3 +2,5 @@ export { default as LeftChevron } from './LeftChevron';
 export { default as Login } from './Login';
 export { default as Logout } from './Logout';
 export { default as Logo } from './Logo';
+export { default as Check } from './Check';
+export { default as Warning } from './Warning';
