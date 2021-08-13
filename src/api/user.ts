78ce81d27bed9fe6b6206bb21aff45aa01ffd74e
parkeunsik <qkrdmstlr3@naver.com>
2021-08-13 21:40:24 +0900

    [feat] [#28] signin api구현 및 붙이기
    - signin api 구현 후 템플릿에 붙힘
    - 에러 컨트롤 필요

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,5 +1,22 @@
 import axios from 'axios';
 import { SignupInfoType } from '../validator/signupValidator';
+import { LoginInfoType } from '../validator/loginValidator';
+
+export interface ErrorType {
+  error: string;
+}
+
+export interface SigninResponseType {
+  access_token: string;
+  refresh_token: string;
+  email: string;
+  token_type: string;
+  user_id: string;
+}
+
+export const isFailed = <T>(arg: T | ErrorType): arg is ErrorType => {
+  return (arg as ErrorType).error !== undefined;
+};
 
 // FIXME: 백엔드쪽 api구현이 완료되지않아 추후 수정 필요
 export const signupAPI = async ({ id, pw, nickname }: SignupInfoType) => {
@@ -13,4 +30,20 @@ export const signupAPI = async ({ id, pw, nickname }: SignupInfoType) => {
     },
   });
   return response;
+};
+
+export const signinAPI = async ({ id, pw }: LoginInfoType): Promise<ErrorType | SigninResponseType> => {
+  try {
+    const response = await axios({
+      method: 'POST',
+      url: '/v1/login',
+      data: {
+        email: id,
+        password: pw,
+      },
+    });
+    return response.data as SigninResponseType;
+  } catch (error) {
+    return { error: '로그인 실패' } as ErrorType;
+  }
 };
