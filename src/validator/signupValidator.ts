d7800610e1373499375c2eb230cbe8289742486d
parkeunsik <qkrdmstlr3@naver.com>
2021-08-15 19:33:23 +0900

    [test] [#26] signup page 테스트 코드 작성
    - 회원가입 페이지 테스트 코드 작성

--- src/validator/signupValidator.ts
+++ src/validator/signupValidator.ts
@@ -6,7 +6,7 @@ export interface SignupInfoType {
 }
 
 const signupValidator = ({ id, pw, confirmPW, nickname }: SignupInfoType) => {
-  if (id.length < 5) return '아이디는 5자리 이상이어야 합니다';
+  if (!id.length) return '이메일을 입력해주십시오';
   if (nickname.length < 2) return '별명은 2자리 이상이어야 합니다';
   if (pw.length < 6 || pw.replace(/[a-zA-Z0-9]+/g, '').length)
     return '비밀번호는 6자리 이상이고 영어와 숫자로만 구성되어야 합니다';
