b20bb14f1df4342cbb13c90830de7cc344a1f3df
parkeunsik <qkrdmstlr3@naver.com>
2021-08-18 00:40:35 +0900

    [feat] [#34] textarea에 size추가
    - textarea Atom도 글자크기를 조절할 수 있도록 type을 주었다

--- src/components/UI/atom/index.ts
+++ src/components/UI/atom/index.ts
@@ -6,3 +6,4 @@ export { default as Input } from './Input';
 export { default as Label } from './Label';
 export { default as Tag } from './Tag';
 export { default as Title } from './Title';
+export { default as Textarea } from './TextArea';
