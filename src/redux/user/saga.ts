23d3df2bab26e03281b459d21bda794d73764e0a
parkeunsik <qkrdmstlr3@naver.com>
2021-08-19 21:26:20 +0900

    [feat] [#35] 로그아웃 redux동작 구현
    - 로그아웃 시 쿠키, 유저정보 초기화

--- src/redux/user/saga.ts
+++ src/redux/user/saga.ts
@@ -1,10 +1,11 @@
 import { PayloadAction } from '@reduxjs/toolkit';
-import { call, put, takeLatest } from 'redux-saga/effects';
+import { call, put, takeLatest, select } from 'redux-saga/effects';
 import { actions } from '.';
-import { signinAPI, SigninResponseType } from '../../api/user';
+import { signinAPI, SigninResponseType, logoutAPI } from '../../api/user';
 import { LoginInfoType } from '../../validator/loginValidator';
 import { ErrorType, isFailed } from '../../api/error';
-import { setCookie } from '../../utils/cookie';
+import { setCookie, getCookie, removeCookie } from '../../utils/cookie';
+import { RootState } from '../store';
 
 function* signin(action: PayloadAction<LoginInfoType>) {
   const result: ErrorType | SigninResponseType = yield call(signinAPI, action.payload);
@@ -13,9 +14,28 @@ function* signin(action: PayloadAction<LoginInfoType>) {
     return;
   }
   yield put(actions.loginSuccess(result));
-  setCookie('soc', result.access_token);
+  setCookie('soca', result.access_token);
+  setCookie('socu', result.uuid);
+}
+
+function* logout() {
+  const accessToken = getCookie('soca');
+  const uuid = getCookie('socu');
+  const email: string = yield select((state: RootState) => state.user.email);
+  yield call(logoutAPI, {
+    accessToken,
+    uuid,
+    email,
+  });
+  yield put(actions.logoutSuccess());
+  removeCookie('soca');
+  removeCookie('socu');
 }
 
 export function* watchSignin() {
   yield takeLatest(actions.loginRequest.type, signin);
+}
+
+export function* watchLogout() {
+  yield takeLatest(actions.logoutRequest.type, logout);
 }
