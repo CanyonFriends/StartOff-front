554484a39154d573cf953ed1b3e023c82c8e44e0
parkeunsik <qkrdmstlr3@naver.com>
2021-08-19 21:26:20 +0900

    [feat] [#35] 로그아웃 redux동작 구현
    - 로그아웃 시 쿠키, 유저정보 초기화

--- src/redux/user/reducer.ts
+++ src/redux/user/reducer.ts
@@ -4,17 +4,28 @@ import { LoginInfoType } from '../../validator/loginValidator';
 import { SigninResponseType } from '../../api/user';
 import { ErrorType } from '../../api/error';
 
+// login
 export const loginRequest = (state: UserState, _: PayloadAction<LoginInfoType>) => {
   state.error = '';
 };
 
 export const loginSuccess = (state: UserState, action: PayloadAction<SigninResponseType>) => {
   state.email = action.payload.email;
-  // state.nickname = action.payload.nickname;
+  state.nickname = action.payload.nickname;
   state.userId = action.payload.user_id;
   state.isSignin = true;
 };
 
 export const loginFailure = (state: UserState, action: PayloadAction<ErrorType>) => {
   state.error = action.payload.errorMsg;
+};
+
+// logout
+export const logoutRequest = (_: UserState) => {};
+
+export const logoutSuccess = (state: UserState) => {
+  state.email = '';
+  state.nickname = '';
+  state.userId = '';
+  state.isSignin = false;
 };
