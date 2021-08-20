6a0d69e87ec19cfc36899fcb2b791f4f8ac21b10
parkeunsik <qkrdmstlr3@naver.com>
2021-08-20 20:22:56 +0900

    [feat] [#29] CheckUser component구현
    - 새로고침했었을 때 api를 요청하는 컴포넌트 구현
    - 새로고침할 때만 동작하면 됨으로 라우팅위에 구현하였음

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-
+import CheckUser from './utils/CheckUser';
 import * as Routes from './Routes';
 import HomePage from './pages/Home';
 import SigninPage from './pages/Signin';
@@ -9,6 +9,7 @@ import SignupPage from './pages/Signup';
 function App() {
   return (
     <>
+      <CheckUser />
       <Switch>
         <Route exact path={Routes.homePath} component={HomePage} />
         <Route path={Routes.signinPath} component={SigninPage} />
