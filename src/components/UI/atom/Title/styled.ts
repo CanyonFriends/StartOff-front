3e841c209a87f7811dff842dcf069d7de37b6357
parkeunsik <qkrdmstlr3@naver.com>
2021-08-21 16:40:48 +0900

    [refactor] button에 type추가 / title margin제거
    - 아톰 컴포넌트들 리팩토링
      - 버튼에 타입 추가
      - 제목에 마진 제거

--- src/components/UI/atom/Title/styled.ts
+++ src/components/UI/atom/Title/styled.ts
@@ -9,29 +9,35 @@ interface TitleStyleProps {
 
 export const Title1 = styled.h1<TitleStyleProps>`
   color: ${(props) => props.color};
+  margin: 0;
   ${(props) => titleSizeSelector[props.fontsize]}
 `;
 export const Title2 = styled.h2<TitleStyleProps>`
   color: ${(props) => props.color};
+  margin: 0;
   ${(props) => titleSizeSelector[props.fontsize]}
 `;
 
 export const Title3 = styled.h3<TitleStyleProps>`
   color: ${(props) => props.color};
+  margin: 0;
   ${(props) => titleSizeSelector[props.fontsize]}
 `;
 
 export const Title4 = styled.h4<TitleStyleProps>`
   color: ${(props) => props.color};
+  margin: 0;
   ${(props) => titleSizeSelector[props.fontsize]}
 `;
 
 export const Title5 = styled.h5<TitleStyleProps>`
   color: ${(props) => props.color};
+  margin: 0;
   ${(props) => titleSizeSelector[props.fontsize]}
 `;
 
 export const Title6 = styled.h6<TitleStyleProps>`
   color: ${(props) => props.color};
+  margin: 0;
   ${(props) => titleSizeSelector[props.fontsize]}
 `;
