f1e568a0f9b508e7efd692a034cd504acf198993
parkeunsik <qkrdmstlr3@naver.com>
2021-08-23 22:54:43 +0900

    [feat] [#37] getprofile api구현 및 타입 설정
    - 클라이언트와 서버 별 타입 설정
    - getProfile api구현

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -2,7 +2,17 @@ import React from 'react';
 import ProfileTemplate from './template';
 
 function Profile() {
-  return <ProfileTemplate />;
+  return (
+    <ProfileTemplate
+      editableAuthority
+      nickname="nickname"
+      introduce=""
+      imageUrl=""
+      github=""
+      blog=""
+      tagContents={[]}
+    />
+  );
 }
 
 export default Profile;
