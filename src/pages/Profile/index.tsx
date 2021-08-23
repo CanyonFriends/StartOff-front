1bc404ee94d67d4abdae9151e45ecc2aefd88874
parkeunsik <qkrdmstlr3@naver.com>
2021-08-24 01:46:43 +0900

    [feat] [#37] 프로필 페이지에서 템플릿에 ui정보 넘기도록 구현
    - getProfile api사용
      - 에러 발생시 모달로 표기
      - image는 아직 미구현

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,17 +1,59 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { getProfileAPI } from '../../api/profile';
 import ProfileTemplate from './template';
+import { ProfileType } from '../../@types/client';
+import { isFailed } from '../../api/error';
+import { RootState } from '../../redux/store';
+import { UserState } from '../../redux/user/types';
+import { AlertModal } from '../../components/UI/organism';
+
+interface ParamProps {
+  userId: string;
+}
 
 function Profile() {
+  const [error, setError] = useState('');
+  const [profileData, setProfileData] = useState<ProfileType | undefined>();
+  const userState = useSelector<RootState>((state) => state.user) as UserState;
+  const { userId } = useParams<ParamProps>();
+  const editableAuthority = userState.userId === userId;
+
+  useEffect(() => {
+    getProfileInfo();
+  }, []);
+
+  const getProfileInfo = async () => {
+    const response = await getProfileAPI({ userId });
+    if (isFailed<ProfileType>(response)) {
+      return '';
+    }
+    setProfileData(response);
+    return '';
+  };
+
+  const handleModalCloseButton = () => {
+    setError('');
+  };
+
   return (
-    <ProfileTemplate
-      editableAuthority
-      nickname="nickname"
-      introduce=""
-      imageUrl=""
-      github=""
-      blog=""
-      tagContents={[]}
-    />
+    <>
+      {!!error.length && <AlertModal content={error} clickCloseButton={handleModalCloseButton} />}
+      {profileData ? (
+        <ProfileTemplate
+          editableAuthority={editableAuthority}
+          nickname={profileData.nickname}
+          introduce={profileData.introduce}
+          imageUrl=""
+          github={profileData.githubUrl}
+          blog={profileData.blogUrl}
+          tagContents={profileData.userSkills}
+        />
+      ) : (
+        <></>
+      )}
+    </>
   );
 }
 
