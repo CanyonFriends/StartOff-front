9675a7f7140703bf7801aee505d025b3f05448fb
parkeunsik <qkrdmstlr3@naver.com>
2021-08-23 22:54:43 +0900

    [feat] [#37] getprofile api구현 및 타입 설정
    - 클라이언트와 서버 별 타입 설정
    - getProfile api구현

---
+++ src/@types/client.ts
@@ -0,0 +1,28 @@
+/**
+ * client data type
+ */
+export interface ProfileType {
+  baekjoonId: string;
+  blogUrl: string;
+  githubUrl: string;
+  introduce: string;
+  nickname: string;
+  projects: ProjectType[];
+  userSkills: SkillType[];
+}
+
+export interface ProjectType {
+  content: string;
+  deployUrl: string;
+  endDate: string;
+  githubUrl: string;
+  id: number;
+  introduce: string;
+  projectSklls: SkillType[];
+}
+
+export interface SkillType {
+  color: string;
+  skillName: string;
+  textColor: string;
+}
---
+++ src/@types/server.ts
@@ -0,0 +1,20 @@
+/**
+ * server data type
+ */
+export interface ProjectServerType {
+  content: string;
+  deploy_url: string;
+  end_date: string;
+  github_url: string;
+  id: number;
+  introduce: string;
+  project_skills: SkillServerType[];
+  start_date: string;
+  title: string;
+}
+
+export interface SkillServerType {
+  color: string;
+  skill_name: string;
+  text_color: string;
+}
