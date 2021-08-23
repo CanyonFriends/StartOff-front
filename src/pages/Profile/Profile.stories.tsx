afcf07929cbe4b52b1213d4d2fd00eec88b34013
parkeunsik <qkrdmstlr3@naver.com>
2021-08-23 12:24:25 +0900

    [feat] [#37] 프로필 페이지 상단 구현
    - 프로필 페이지에서 프로젝트부분을 제외한 부분 구현

--- src/pages/Profile/Profile.stories.tsx
+++ src/pages/Profile/Profile.stories.tsx
@@ -1,11 +1,41 @@
 import React from 'react';
 import ProfileTemplate from './template';
+import { tags } from '../../__test__/mock-dats';
 
 export default {
   title: 'Template/Profile',
   component: ProfileTemplate,
 };
 
-export const profile = (): React.ReactElement => {
-  return <ProfileTemplate />;
+export const editableProfile = (): React.ReactElement => {
+  const github = 'https://github.com/qkrdmstlr3';
+  const blog = 'https://shellboylog.com';
+  return (
+    <ProfileTemplate
+      editableAuthority
+      github={github}
+      blog={blog}
+      nickname="nickname"
+      introduce="introduce"
+      imageUrl=""
+      tagContents={tags}
+    />
+  );
+};
+
+export const uneditableProfile = (): React.ReactElement => {
+  const github = 'https://github.com/qkrdmstlr3';
+  const blog = 'https://shellboylog.com';
+
+  return (
+    <ProfileTemplate
+      editableAuthority={false}
+      github={github}
+      blog={blog}
+      nickname="nickname"
+      introduce="introduce"
+      imageUrl=""
+      tagContents={tags}
+    />
+  );
 };
