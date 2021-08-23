6f7c22d2507d6e6f4b63036399d537918c3484c6
parkeunsik <qkrdmstlr3@naver.com>
2021-08-23 12:24:25 +0900

    [feat] [#37] 프로필 페이지 상단 구현
    - 프로필 페이지에서 프로젝트부분을 제외한 부분 구현

--- src/pages/Profile/template/index.tsx
+++ src/pages/Profile/template/index.tsx
@@ -1,7 +1,90 @@
 import React from 'react';
+import * as Style from './styled';
+import CommonHeader from '../../../components/Layout/CommonHeader';
+import { ProfileIntroduce, ProfileInfoCard, ProfileTagCard } from '../../../components/UI/organism';
+import { ModifyProfileInfoCardType } from '../../../validator/modifyProfileInfoCard';
+import { ModifyProfileIntroduceType } from '../../../validator/modifyProfileIntroduce';
+import { ProfileInfoCardProps } from '../../../components/UI/organism/ProfileInfoCard';
+import { ProfileTagCardProps } from '../../../components/UI/organism/ProfileTagCard/index';
+import { TagProps } from '../../../components/UI/atom/Tag/index';
 
-function ProfileTemplate() {
-  return <></>;
+interface ProfileTemplateProps {
+  editableAuthority: boolean;
+  nickname: string;
+  introduce: string;
+  imageUrl: string;
+  github: string;
+  blog: string;
+  tagContents: TagProps[];
+}
+
+function ProfileTemplate({
+  editableAuthority,
+  nickname,
+  introduce,
+  imageUrl,
+  github,
+  blog,
+  tagContents,
+}: ProfileTemplateProps) {
+  const handleSubmitIntroduce = async (data: ModifyProfileIntroduceType) => {
+    return '';
+  };
+
+  const handleSubmitGithub = async (data: ModifyProfileInfoCardType) => {
+    return '';
+  };
+
+  const handleSubmitBlog = async (data: ModifyProfileInfoCardType) => {
+    return '';
+  };
+
+  const githubInfo: ProfileInfoCardProps = {
+    editableAuthority,
+    title: 'Github',
+    textValue: github,
+    iconType: 'Logo',
+    handleSubmit: handleSubmitGithub,
+  };
+  const blogInfo: ProfileInfoCardProps = {
+    editableAuthority,
+    title: 'Blog',
+    textValue: blog,
+    iconType: 'Home',
+    handleSubmit: handleSubmitBlog,
+  };
+  const tagInfo: ProfileTagCardProps = {
+    editableAuthority,
+    tagContents,
+    title: '기술 스택',
+  };
+  return (
+    <>
+      <CommonHeader />
+      <Style.Container>
+        <Style.ProfileHeaderWrapper>
+          <ProfileIntroduce
+            editableAuthority={editableAuthority}
+            nickname={nickname}
+            introduce={introduce}
+            imageurl={imageUrl}
+            handleSubmit={handleSubmitIntroduce}
+          />
+        </Style.ProfileHeaderWrapper>
+        <Style.ProfileInfoWrapper>
+          <ProfileInfoCard {...githubInfo} />
+          <ProfileInfoCard {...blogInfo} />
+          <ProfileTagCard {...tagInfo} />
+        </Style.ProfileInfoWrapper>
+        <Style.ProfileImageWrapper>
+          <></>
+        </Style.ProfileImageWrapper>
+        <Style.ProfileProjectsWrapper>
+          <></>
+        </Style.ProfileProjectsWrapper>
+      </Style.Container>
+    </>
+  );
 }
 
 export default ProfileTemplate;
