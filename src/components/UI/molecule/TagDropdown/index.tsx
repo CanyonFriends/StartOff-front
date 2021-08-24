a7df4bf721800d63547bc382d6fcbf4252e709d5
parkeunsik <qkrdmstlr3@naver.com>
2021-08-24 22:07:45 +0900

    [feat] [#13] TagDropdown molecule 구현
    - 태그들을 보여줄 molecule을 구현하였다.
    - 다른 드롭다운 필요시 확장할 수 있을 것 같다

---
+++ src/components/UI/molecule/TagDropdown/index.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import Overlay from '../../../Layout/Overlay';
+import { Button } from '../../atom';
+import * as Style from './styled';
+
+export interface DropdownTagType {
+  id: string;
+  content: string;
+}
+
+export interface TagDropdownProps {
+  placeholder: string;
+  tags: DropdownTagType[];
+  clickItem: (tagId: string) => void;
+}
+
+function TagDropdown({ placeholder, tags, clickItem }: TagDropdownProps) {
+  const [open, setOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setOpen(!open);
+  };
+
+  return (
+    <Style.Container>
+      <Button size="medium" onClick={toggleDropdown}>
+        {placeholder}
+      </Button>
+      {open && (
+        <Style.MenuWrapper>
+          <Overlay clickModalOutside={toggleDropdown} />
+          {tags.map((tag) => (
+            <Style.TagItem id={tag.id} onClick={() => clickItem(tag.id)}>
+              {tag}
+            </Style.TagItem>
+          ))}
+        </Style.MenuWrapper>
+      )}
+    </Style.Container>
+  );
+}
+
+export default TagDropdown;
