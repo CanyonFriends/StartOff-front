791b7120c153d2f9efd5475e1777fa865453c5ac
parkeunsik <qkrdmstlr3@naver.com>
2021-08-24 22:08:32 +0900

    [refactor] [#21] ProfileTagCard제출방식 변경
    - 아이콘 제거
    - 드롭다운 추가로 인해서 드롭다운의 아이템을 누르면 변경되도록 하였다

--- src/components/UI/organism/ProfileTagCard/ProfileTagCard.stories.tsx
+++ src/components/UI/organism/ProfileTagCard/ProfileTagCard.stories.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { withKnobs, text } from '@storybook/addon-knobs';
+import { action } from '@storybook/addon-actions';
 import ProfileTagCard from '.';
 import { tags } from '../../../../__test__/mock-dats';
 
@@ -9,14 +10,16 @@ export default {
   decorators: [withKnobs],
 };
 
+const onClickAction = action('click');
+
 export const editable = (): React.ReactElement => {
   const title = text('TITLE', '주요기술');
 
-  return <ProfileTagCard editableAuthority title={title} tagContents={tags} />;
+  return <ProfileTagCard editableAuthority clickTagItem={onClickAction} title={title} tagContents={tags} />;
 };
 
 export const uneditable = (): React.ReactElement => {
   const title = text('TITLE', '주요기술');
 
-  return <ProfileTagCard editableAuthority={false} title={title} tagContents={tags} />;
+  return <ProfileTagCard editableAuthority={false} clickTagItem={onClickAction} title={title} tagContents={tags} />;
 };
