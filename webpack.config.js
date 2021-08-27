f9d11d69686365317601bd67c6bd96f184337956
parkeunsik <qkrdmstlr3@naver.com>
2021-08-27 14:22:00 +0900

    [chore] webpack css 설정
    - react-datepicker의 css파일 사용을 위해서 설정하였다

--- webpack.config.js
+++ webpack.config.js
@@ -25,6 +25,10 @@ module.exports = {
         test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
         use: ['file-loader'],
       },
+      {
+        test: /\.css$/,
+        use: ['css-loader'],
+      },
     ],
   },
   plugins: [
