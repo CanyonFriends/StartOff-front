2dace9c73666105f69ab7bc3ce5c293d5b0ef553
parkeunsik <qkrdmstlr3@naver.com>
2021-08-27 13:43:25 +0900

    [refactor] [#34] Label Atom 굵기, 크기 수정
    - 굵기 속성 추가
    - 폰트 사이즈 조정

--- src/components/UI/atom/Label/styled.ts
+++ src/components/UI/atom/Label/styled.ts
@@ -5,10 +5,12 @@ import { labelSizeSelector } from './style-selector';
 interface LabelStyleProps {
   color: string;
   size: SizeType;
+  bolder: boolean;
 }
 
 export const Container = styled.label<LabelStyleProps>`
   color: ${(props) => props.color};
+  font-weight: ${(props) => (props.bolder ? 'bolder' : 'normal')};
 
   ${(props) => labelSizeSelector[props.size]}
 `;
