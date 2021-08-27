a40d24f38f9792e7e86dc5d524a63f4660821f5d
parkeunsik <qkrdmstlr3@naver.com>
2021-08-27 16:08:45 +0900

    [feat] [#34] Icon에 Plus 추가
    - plus svg를 icon에 추가한다

--- src/components/UI/atom/Icon/svg/index.ts
+++ src/components/UI/atom/Icon/svg/index.ts
@@ -8,3 +8,4 @@ export { default as Profile } from './Profile';
 export { default as Pencil } from './Pencil';
 export { default as Disk } from './Disk';
 export { default as Home } from './Home';
+export { default as Plus } from './Plus';
