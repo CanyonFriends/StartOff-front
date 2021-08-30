31083b4b6334dcee32bae4902030227963bad523
parkeunsik <qkrdmstlr3@naver.com>
2021-08-31 00:37:52 +0900

    [feat] [#13] Dropdown Molecule구현
    - 드롭다운 역할을 할 molecule구현

---
+++ src/components/UI/molecule/Dropdown/index.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import Overlay from '../../../Layout/Overlay';
+import { Button } from '../../atom';
+import * as Style from './styled';
+
+interface ItemProps {
+  id: string;
+  text: string;
+}
+
+export interface DropdownProps {
+  placeholder: string;
+  items: ItemProps[];
+  clickItem: (id: string) => void;
+}
+
+function Dropdown({ placeholder, items, clickItem }: DropdownProps) {
+  const [open, setOpen] = useState(false);
+
+  const toggleDropdown = () => {
+    setOpen(!open);
+  };
+
+  const handleClickItem = (skillName: string) => {
+    clickItem(skillName);
+    toggleDropdown();
+  };
+
+  return (
+    <Style.Container>
+      <Button formButton={false} size="medium" onClick={toggleDropdown}>
+        {placeholder}
+      </Button>
+      {open && (
+        <Style.MenuWrapper>
+          <Overlay clickModalOutside={toggleDropdown} />
+          {items.map((item) => (
+            <Style.Item key={item.id} id={item.id} onClick={() => handleClickItem(item.text)}>
+              {item.text}
+            </Style.Item>
+          ))}
+        </Style.MenuWrapper>
+      )}
+    </Style.Container>
+  );
+}
+
+export default Dropdown;
