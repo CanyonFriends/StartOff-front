1c2b398af48f4ee690778c3cdb4730edc7987d94
parkeunsik <qkrdmstlr3@naver.com>
2021-08-30 21:07:14 +0900

    [feat] [#40] 스터디 페이지 기본 파일들 생성
    - 스터디 페이지들에 대한 파일들을 생성

---
+++ src/pages/StudyBoard/StudyBoard.stories.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import StudyBoardTemplate from './template';
+
+export default {
+  title: 'Template/StudyBoard',
+  component: StudyBoardTemplate,
+};
+
+export const studyboardTemplate = (): React.ReactElement => {
+  return <StudyBoardTemplate />;
+};
