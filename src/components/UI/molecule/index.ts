598efd33a5d7ef886a028e9956eda0826e3fa437
parkeunsik <qkrdmstlr3@naver.com>
2021-08-31 00:20:55 +0900

    [feat] [#13] SearchBox Molecule 구현
    - 검색창의 역할을 할 molecule을 구현하였다
      - 스토리 파일 구현

--- src/components/UI/molecule/index.ts
+++ src/components/UI/molecule/index.ts
@@ -3,3 +3,4 @@ export { default as BoxWithIcon } from './BoxWithIcon';
 export { default as InputField } from './InputField';
 export { default as EditableText } from './EditableText';
 export { default as TextAreaField } from './TextAreaField';
+export { default as SearchBox } from './SearchBox';
