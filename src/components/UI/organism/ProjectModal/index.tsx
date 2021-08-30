8b128de84e9827e1272b47390df34d665fab3f08
parkeunsik <qkrdmstlr3@naver.com>
2021-08-30 12:40:25 +0900

    [test] aria-label 할당
    - 테스트 코드 구현을 위해서 tag를 가져오기 위한 aria-label을 할당해주었다

--- src/components/UI/organism/ProjectModal/index.tsx
+++ src/components/UI/organism/ProjectModal/index.tsx
@@ -93,6 +93,7 @@ function ProjectModal({ isModify, project, totalSkillList, onSubmit, handleModal
     sortDirection: 'column',
     labelProps: { content: '프로젝트명', color: theme.color.color_primary_400, bolder: true, size: 'medium' },
     inputProps: {
+      id: 'project-title',
       value: values.title,
       onChange: handleTitleChange,
       placeholder: '프로젝트명을 입력해주세요',
@@ -105,6 +106,7 @@ function ProjectModal({ isModify, project, totalSkillList, onSubmit, handleModal
     sortDirection: 'column',
     labelProps: { content: '한 줄 소개(100자)', color: theme.color.color_primary_400, bolder: true, size: 'medium' },
     inputProps: {
+      id: 'project-introduce',
       value: values.introduce,
       onChange: handleIntroduceChange,
       placeholder: '프로젝트 한 줄로 요약',
@@ -117,6 +119,7 @@ function ProjectModal({ isModify, project, totalSkillList, onSubmit, handleModal
     sortDirection: 'column',
     labelProps: { content: '깃허브 주소', color: theme.color.color_primary_400, bolder: true, size: 'medium' },
     inputProps: {
+      id: 'project-github',
       value: values.githubUrl,
       onChange: handleGithubChange,
       placeholder: '깃허브 주소를 입력해주세요',
@@ -129,6 +132,7 @@ function ProjectModal({ isModify, project, totalSkillList, onSubmit, handleModal
     sortDirection: 'column',
     labelProps: { content: '배포 주소', color: theme.color.color_primary_400, bolder: true, size: 'medium' },
     inputProps: {
+      id: 'project-deploy',
       value: values.deployUrl,
       onChange: handleDeployChange,
       placeholder: '배포 주소를 입력해주세요',
@@ -141,6 +145,7 @@ function ProjectModal({ isModify, project, totalSkillList, onSubmit, handleModal
     sortDirection: 'row',
     labelProps: { content: '진행중', color: theme.color.color_primary_400, bolder: true, size: 'medium' },
     inputProps: {
+      id: 'project-calendar',
       value: '진행중',
       width: 'auto',
       checked: values.isProgress,
@@ -160,7 +165,13 @@ function ProjectModal({ isModify, project, totalSkillList, onSubmit, handleModal
     group: 'content',
     sortDirection: 'column',
     labelProps: { content: '프로젝트 설명(200자)', color: theme.color.color_primary_400, bolder: true, size: 'medium' },
-    textareaProps: { value: values.content, onChange: handleContentChange, placeholder: '내용 입력', size: 'small' },
+    textareaProps: {
+      id: 'project-content',
+      value: values.content,
+      onChange: handleContentChange,
+      placeholder: '내용 입력',
+      size: 'small',
+    },
   };
 
   return (
