e89415729061be81d5b1e59dda64e264290dc027
parkeunsik <qkrdmstlr3@naver.com>
2021-08-31 16:46:42 +0900

    [test] validator 파일들 테스트코드 작성
    - validator파일들에 대한 테스트코드 작성
    - 올바르지 않은 파일명들 변경
    - 올바르지 않은 조건들 수정

---
+++ src/components/__test__/README.md
@@ -0,0 +1,1 @@
+Layout과 Organism에 대한 테스트코드
--- src/validator/updatePasswordValidator.ts
+++ src/validator/updatePasswordValidator.ts
@@ -6,7 +6,8 @@ export interface UpdatePasswordValidatorType {
 
 const updatePasswordValidator = ({ currentPW, afterPW, confirmPW }: UpdatePasswordValidatorType) => {
   if (!currentPW.length) return '현재 비밀번호를 입력해 주십시오';
-  if (!afterPW.length) return '비밀번호는 6자리 이상이고 영어와 숫자로만 구성되어야 합니다';
+  if (afterPW.length < 6 || afterPW.replace(/[a-zA-Z0-9]+/g, '').length)
+    return '비밀번호는 6자리 이상이고 영어와 숫자로만 구성되어야 합니다';
   if (afterPW !== confirmPW) return '비밀번호가 서로 다릅니다';
   return '';
 };
