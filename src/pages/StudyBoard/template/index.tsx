574e2eac627306595d616b912241059a84f3fd35
parkeunsik <qkrdmstlr3@naver.com>
2021-09-07 22:08:03 +0900

    [feat | test] 글쓰기 버튼 클릭 시 라우팅
    - 로그인한 상태라면 글쓰기 페이지로 라우팅
    - 그렇지 않다면 에러 모달 렌더링

--- src/pages/StudyBoard/template/index.tsx
+++ src/pages/StudyBoard/template/index.tsx
@@ -1,24 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import * as Style from './styled';
 import { SummarizedPostClientType } from '../../../@types/client';
 import CommonHeader from '../../../components/Layout/CommonHeader';
 import { Button } from '../../../components/UI/atom';
 import { Dropdown, SearchBox } from '../../../components/UI/molecule';
-import { SummarizedPostItem } from '../../../components/UI/organism';
+import { AlertModal, SummarizedPostItem } from '../../../components/UI/organism';
+import { RootState } from '../../../redux/store';
+import { UserState } from '../../../redux/user/types';
+import { createPostPath } from '../../../Routes';
 
 interface StudyBoardTemplateProps {
   posts: SummarizedPostClientType[];
 }
 
 function StudyBoardTemplate({ posts }: StudyBoardTemplateProps) {
+  const history = useHistory();
+  const [error, setError] = useState('');
+  const { isSignin } = useSelector<RootState>((state) => state.user) as UserState;
+
   const clickDropdownItem = () => {};
 
   const searchSubmit = async () => {
     return '';
   };
 
+  const clickCreatePostButton = () => {
+    if (!isSignin) {
+      setError('로그인이 필요합니다');
+      return;
+    }
+
+    history.push(createPostPath);
+  };
+
+  const closeAlertModal = () => {
+    setError('');
+  };
+
   return (
     <>
+      {!!error && <AlertModal content={error} clickCloseButton={closeAlertModal} />}
       <CommonHeader />
       <Style.Container>
         <Style.Header>
@@ -26,7 +49,7 @@ function StudyBoardTemplate({ posts }: StudyBoardTemplateProps) {
             <Dropdown placeholder="제목" items={[]} clickItem={clickDropdownItem} />
             <SearchBox searchSubmit={searchSubmit} />
           </Style.HeaderLeft>
-          <Button theme="secondary" size="medium">
+          <Button theme="secondary" size="medium" onClick={clickCreatePostButton}>
             글 쓰기
           </Button>
         </Style.Header>
--- src/pages/__test__/StudyBoard.test.tsx
+++ src/pages/__test__/StudyBoard.test.tsx
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 import React from 'react';
-import { waitFor } from '@testing-library/react';
+import { waitFor, fireEvent } from '@testing-library/react';
 import { render } from '../../test-utils';
 import StudyBoardPage from '../StudyBoard';
 
@@ -21,5 +21,14 @@ describe('<StudyBoard> 페이지', () => {
 
     component.getByText('글 쓰기');
     component.getByText('검색');
+  });
+
+  it('로그인 없이 글 쓰기 버튼 클릭 시 에러', async () => {
+    const component = render(<StudyBoardPage />);
+
+    const createPostButton = component.getByText('글 쓰기');
+    fireEvent.click(createPostButton);
+
+    await component.findByText('로그인이 필요합니다');
   });
 });
