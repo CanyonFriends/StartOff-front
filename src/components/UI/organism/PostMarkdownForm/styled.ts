ea16314c6d5b721503db1acb56f3b9bedd8e5a80
parkeunsik <qkrdmstlr3@naver.com>
2021-09-08 00:23:41 +0900

    [feat | Organism] [#21] PostMarkdownForm 구현
    - 포스트 내용의 마크다운 form을 담당하는 컴포넌트 구현
      - 스토리 파일 구현
      - 테스트 코드 작성

---
+++ src/components/UI/organism/PostMarkdownForm/styled.ts
@@ -0,0 +1,24 @@
+import styled from '@emotion/styled';
+
+export const Header = styled.header`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  font-size: 2rem;
+  font-weight: bold;
+`;
+
+export const Container = styled.div`
+  font-size: 1.8rem;
+  display: flex;
+  height: 100%;
+`;
+
+export const TextareaContainer = styled.div`
+  width: 50%;
+`;
+
+export const MarkdownContainer = styled.div`
+  width: 50%;
+  border: 1px solid black;
+`;
