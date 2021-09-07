b83bb4e8a634a0b759ffc987550e39c7e1425e80
parkeunsik <qkrdmstlr3@naver.com>
2021-09-07 22:00:29 +0900

    [feat] CreatePost 페이지 목업 구현
    - CreatePost 템플릿과 페이지 구현
    - 스토리 파일 생성
    - 테스트 코드 생성

---
+++ src/pages/CreatePost/CreatePost.stories.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import CreatePostTemplate from './template';
+
+export default {
+  title: 'Template/CreatePost',
+  component: CreatePostTemplate,
+};
+
+export const createPostTemplate = (): React.ReactElement => <CreatePostTemplate />;
