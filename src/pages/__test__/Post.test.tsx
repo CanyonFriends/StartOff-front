0027eda42819c206c05fe31c7fe5c2d13ea9c069
parkeunsik <qkrdmstlr3@naver.com>
2021-09-09 00:27:46 +0900

    [test] page테스트 코드에 getCategoriesMockAPI추가
    - CommonHeader에서 공통적으로 호출하는 getCategoriesMockAPI를 추가하였다

--- src/pages/__test__/Post.test.tsx
+++ src/pages/__test__/Post.test.tsx
@@ -5,9 +5,15 @@ import React from 'react';
 import { waitFor } from '@testing-library/react';
 import { render } from '../../test-utils';
 import Post from '../Post';
+import { getCategoriesAPI } from '../../api/post';
+
+jest.mock('../../api/post');
+
+const getCategoriesMockAPI = getCategoriesAPI as jest.MockedFunction<typeof getCategoriesAPI>;
 
 beforeEach(() => {
   jest.resetAllMocks();
+  getCategoriesMockAPI.mockReturnValue(new Promise((res) => res([])));
 });
 
 describe('<Post> 페이지', () => {
