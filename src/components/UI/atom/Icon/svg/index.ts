2b22e0a85d80638ac4deee728e7730637d8bfbbd
parkeunsik <qkrdmstlr3@naver.com>
2021-09-08 13:29:35 +0900

    [feat] [#34] BottomChevron Icon추가
    - 아래쪽 화살표 아이콘 추가

--- src/components/UI/atom/Icon/svg/index.ts
+++ src/components/UI/atom/Icon/svg/index.ts
@@ -11,3 +11,4 @@ export { default as Home } from './Home';
 export { default as Plus } from './Plus';
 export { default as TrashCan } from './TrashCan';
 export { default as Search } from './Search';
+export { default as BottomChevron } from './BottomChevron';
