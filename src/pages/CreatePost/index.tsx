9071872ea70be1d450e8adf72746a31aa96354ff
parkeunsik <qkrdmstlr3@naver.com>
2021-09-08 13:42:55 +0900

    [feat] CreatePost 페이지 구현(api제외)
    - api를 제외한 createPost페이지를 구현하였다

--- src/pages/CreatePost/index.tsx
+++ src/pages/CreatePost/index.tsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { SkillClientType } from '../../@types/client';
+import { isFailed } from '../../api/error';
+import { getSkillsAPI } from '../../api/skill';
 import CreatePostTemplate from './template';
 
 function CreatePost() {
-  return <CreatePostTemplate />;
+  const [totalSkills, setTotalSkills] = useState<SkillClientType[]>([]);
+
+  useEffect(() => {
+    getTotalSkills();
+  }, []);
+
+  const getTotalSkills = async () => {
+    const response = await getSkillsAPI();
+    if (isFailed<SkillClientType[]>(response)) {
+      return;
+    }
+    setTotalSkills(response);
+  };
+
+  return <CreatePostTemplate totalSkillList={totalSkills} />;
 }
 
 export default CreatePost;
