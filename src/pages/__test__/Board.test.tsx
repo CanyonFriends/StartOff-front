e07dd86e674bf37c2aab7569d493d0926e77f4f3
parkeunsik <qkrdmstlr3@naver.com>
2021-09-09 00:28:11 +0900

    [test] Board에서 getPostMockAPI구현
    - Board에서 보드 정보를 가져오는 api의 목업코드를 구현하였다

--- src/pages/__test__/Board.test.tsx
+++ src/pages/__test__/Board.test.tsx
@@ -5,14 +5,17 @@ import React from 'react';
 import { waitFor, fireEvent } from '@testing-library/react';
 import { render } from '../../test-utils';
 import Board from '../Board';
-import { getCategoriesAPI } from '../../api/post';
+import { getCategoriesAPI, getPostsAPI } from '../../api/post';
+import { makeMockBoard } from '../../__mocks__/client-mock-data';
 
 jest.mock('../../api/post');
 
+const getPostsMockAPI = getPostsAPI as jest.MockedFunction<typeof getPostsAPI>;
 const getCategoriesMockAPI = getCategoriesAPI as jest.MockedFunction<typeof getCategoriesAPI>;
 
 beforeEach(() => {
   jest.resetAllMocks();
+  getPostsMockAPI.mockReturnValue(new Promise((res) => res(makeMockBoard({}))));
   getCategoriesMockAPI.mockReturnValue(new Promise((res) => res([])));
 });
 
@@ -25,15 +28,19 @@ describe('<Board> 페이지', () => {
   it('렌더링 테스트', async () => {
     const component = render(<Board />);
 
-    component.getByText('글 쓰기');
-    component.getByText('검색');
+    await waitFor(() => {
+      component.getByText('글 쓰기');
+      component.getByText('검색');
+    });
   });
 
   it('로그인 없이 글 쓰기 버튼 클릭 시 에러', async () => {
     const component = render(<Board />);
 
-    const createPostButton = component.getByText('글 쓰기');
-    fireEvent.click(createPostButton);
+    await waitFor(() => {
+      const createPostButton = component.getByText('글 쓰기');
+      fireEvent.click(createPostButton);
+    });
 
     await component.findByText('로그인이 필요합니다');
   });
