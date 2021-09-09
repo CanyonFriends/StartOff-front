fedcb06840c61dcb590771cfba8da565bdc1ae0d
parkeunsik <qkrdmstlr3@naver.com>
2021-09-10 01:36:12 +0900

    [feat] 게시판 페이지네이션 구현
    - 게시판 페이지에 페이지네이션 컴포넌트를 이용해서 페이지네이션 기능을 구현하였다
    - qs를 바꿔가며 정상적으로 동작하는 것을 확인하였다

--- src/pages/Board/template/BoardTemplate.stories.tsx
+++ src/pages/Board/template/BoardTemplate.stories.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { action } from '@storybook/addon-actions';
 import BoardTemplate from '.';
 import { makeMockSummarizedPost } from '../../../__mocks__/client-mock-data';
 
@@ -7,7 +8,16 @@ export default {
   component: BoardTemplate,
 };
 
+const clickAction = action('click');
 export const boardTemplate = (): React.ReactElement => {
   const summarizedPosts = [makeMockSummarizedPost({}), makeMockSummarizedPost({}), makeMockSummarizedPost({})];
-  return <BoardTemplate posts={summarizedPosts} board="board" />;
+  return (
+    <BoardTemplate
+      posts={summarizedPosts}
+      board="board"
+      totalPage={12}
+      currentPage={1}
+      handlePagination={clickAction}
+    />
+  );
 };
