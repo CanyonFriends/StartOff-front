9af00f19ef972ffba8e52acb58099a4c0d7ac219
parkeunsik <qkrdmstlr3@naver.com>
2021-09-12 16:21:37 +0900

    [feat] [#21] CommentForm Organism 구현
    - 댓글 작성을 위한 form을 구현
      - 테스트 코드 구현
      - 스토리 파일 구현

---
+++ src/validator/commentValidator.ts
@@ -0,0 +1,10 @@
+export interface CommentValidatorType {
+  comment: string;
+}
+
+const commentValidator = ({ comment }: CommentValidatorType) => {
+  if (comment.length < 5) return '댓글은 다섯글자 이상을 입력해야합니다';
+  return '';
+};
+
+export default commentValidator;
