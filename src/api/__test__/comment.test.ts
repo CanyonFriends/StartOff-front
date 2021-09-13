2f07813fb89cf2e4c2fbf8117568b9a88cf48f86
parkeunsik <qkrdmstlr3@naver.com>
2021-09-13 23:53:31 +0900

    [feat] [#56, #57] 댓글 수정/삭제 api구현
    - 댓글을 수정, 삭제 할 수 있는 api함수 두 개 구현
      - 테스트 코드 작성
    - commentMock생성 시 default content 값 변경

--- src/api/__test__/comment.test.ts
+++ src/api/__test__/comment.test.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
-import { createCommentAPI } from '../comment';
+import { createCommentAPI, deleteCommentAPI, updateCommentAPI } from '../comment';
 import { isFailed } from '../error';
 import { makeCommentMock } from '../../__mocks__/server-mock.data';
 import { CommentClientType } from '../../@types/client';
@@ -34,6 +34,49 @@ describe('API/comment', () => {
     });
 
     expect(isFailed<CommentClientType>(response)).toBeTruthy();
+    expect(response).toHaveProperty('error_msg', 'error');
+  });
+
+  it('updateCommentAPI', async () => {
+    const commentMock = makeCommentMock({});
+    mock.onPut('/v1/posts/1/comments/1').reply(200, commentMock);
+    const response = await updateCommentAPI({
+      userId: '1',
+      postId: '1',
+      commentId: '1',
+      content: 'content',
+    });
+
+    expect(isFailed<CommentClientType>(response)).toBeFalsy();
+    expect(commentMock).toHaveProperty('content', commentMock.content);
+  });
+
+  it('updateCommentAPI error', async () => {
+    mock.onPut('/v1/posts/1/comments/1').reply(400, { error_msg: 'error' });
+    const response = await updateCommentAPI({
+      userId: '1',
+      postId: '1',
+      commentId: '1',
+      content: 'content',
+    });
+
+    expect(isFailed<CommentClientType>(response)).toBeTruthy();
+    expect(response).toHaveProperty('error_msg', 'error');
+  });
+
+  it('deleteCommentAPI', async () => {
+    mock.onDelete('v1/posts/1/comments/1').reply(200, true);
+    const response = await deleteCommentAPI({ postId: '1', commentId: '1' });
+
+    expect(isFailed<boolean>(response)).toBeFalsy();
+    expect(response).toBeTruthy();
+  });
+
+  it('deleteCommentAPI error', async () => {
+    mock.onDelete('v1/posts/1/comments/1').reply(400, { error_msg: 'error' });
+    const response = await deleteCommentAPI({ postId: '1', commentId: '1' });
+
+    expect(isFailed<boolean>(response)).toBeTruthy();
     expect(response).toHaveProperty('error_msg', 'error');
   });
 });
--- src/api/comment.ts
+++ src/api/comment.ts
@@ -4,13 +4,25 @@ import axios from '../utils/axios';
 import { CommentServerType } from '../@types/server';
 import { commentServerType2ClientType } from '../converter/comment';
 
-export interface CreatePostRequest {
+export interface CreateCommentRequest {
   postId: string;
   userId: string;
   content: string;
 }
 
-export const createCommentAPI = async ({ postId, userId, content }: CreatePostRequest) => {
+export interface UpdateCommentRequest {
+  postId: string;
+  userId: string;
+  content: string;
+  commentId: string;
+}
+
+export interface DeleteCommentRequest {
+  postId: string;
+  commentId: string;
+}
+
+export const createCommentAPI = async ({ postId, userId, content }: CreateCommentRequest) => {
   try {
     const response = await axios({
       method: 'POST',
@@ -23,6 +35,37 @@ export const createCommentAPI = async ({ postId, userId, content }: CreatePostRe
     });
 
     return commentServerType2ClientType(response.data as CommentServerType);
+  } catch (error) {
+    return { error_msg: (error as AxiosError).response?.data?.error_msg } as ErrorType;
+  }
+};
+
+export const updateCommentAPI = async ({ postId, userId, content, commentId }: UpdateCommentRequest) => {
+  try {
+    const response = await axios({
+      method: 'PUT',
+      url: `/v1/posts/${postId}/comments/${commentId}`,
+      data: {
+        parend_id: null,
+        user_id: userId,
+        content,
+      },
+    });
+
+    return commentServerType2ClientType(response.data as CommentServerType);
+  } catch (error) {
+    return { error_msg: (error as AxiosError).response?.data?.error_msg } as ErrorType;
+  }
+};
+
+export const deleteCommentAPI = async ({ postId, commentId }: DeleteCommentRequest) => {
+  try {
+    await axios({
+      method: 'DELETE',
+      url: `/v1/posts/${postId}/comments/${commentId}`,
+    });
+
+    return true;
   } catch (error) {
     return { error_msg: (error as AxiosError).response?.data?.error_msg } as ErrorType;
   }
