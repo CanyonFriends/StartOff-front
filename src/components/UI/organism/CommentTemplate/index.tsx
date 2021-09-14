39e08bd91f1942fb81015838d71f31c5975e402a
parkeunsik <qkrdmstlr3@naver.com>
2021-09-14 20:55:28 +0900

    [feat] [#56] 댓글 수정 기능 구현
    - 자신의 댓글 수정 기능 구현
    - api를 붙혔다

--- src/components/UI/organism/CommentTemplate/index.tsx
+++ src/components/UI/organism/CommentTemplate/index.tsx
@@ -5,57 +5,80 @@ import { EditableText } from '../../molecule';
 import * as Style from './styled';
 import { dateToString } from '../../../../utils/date';
 import { TextAreaProps } from '../../atom/TextArea';
+import useForm from '../../../../hooks/useForm';
+import commentValidator, { CommentValidatorType } from '../../../../validator/commentValidator';
+import { AlertModal } from '..';
 
 interface CommentTemplateProps {
   comment: CommentClientType;
   editableAuthority: boolean;
   handleDelete: (commentId: string) => void;
+  handleSubmitModify: (value: CommentValidatorType) => Promise<string>;
 }
 
-function CommentTemplate({ comment, editableAuthority, handleDelete }: CommentTemplateProps) {
+function CommentTemplate({ comment, editableAuthority, handleDelete, handleSubmitModify }: CommentTemplateProps) {
   const [commentEditable, setCommentEditable] = useState(false);
+  const { values, handleChange, error, clearError, handleSubmitWithErrorControl } = useForm<CommentValidatorType>({
+    initialState: { commentId: comment.commentId, comment: comment.content },
+    validator: commentValidator,
+    onSubmit: handleSubmitModify,
+  });
 
   const toggleCommentEditable = () => {
     setCommentEditable(!commentEditable);
   };
 
-  const handleContentChange = () => {};
+  const handleContentChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+    handleChange({ ...values, comment: event.target.value });
+  };
 
   const handleClickDeleteButton = () => {
     handleDelete(comment.commentId);
   };
 
+  const handleFormSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    const result = await handleSubmitWithErrorControl(event);
+    if (result) setCommentEditable(false);
+  };
+
+  const handleCloseModal = () => {
+    clearError();
+  };
+
   const textareaProps: TextAreaProps = {
     ariaLabel: 'comment-edit-textarea',
-    value: comment.content,
+    value: values.comment,
     onChange: handleContentChange,
   };
   return (
-    <Style.Container>
-      <Style.Info>
-        <Style.NicknameWrapper>{comment.nickname}</Style.NicknameWrapper>
-        <Style.DateWrapper>{dateToString(comment.createdAt)}</Style.DateWrapper>
-        {editableAuthority && !commentEditable ? (
-          <>
-            <Button formButton={false} onClick={toggleCommentEditable}>
-              수정
-            </Button>
-            <Button formButton={false} theme="secondary" onClick={handleClickDeleteButton}>
-              삭제
-            </Button>
-          </>
-        ) : (
-          <></>
-        )}
-      </Style.Info>
-      <EditableText
-        isEditable={commentEditable}
-        textType="paragraph"
-        paragraphProps={{ size: 'medium', content: '' }}
-        textareaProps={textareaProps}
-      />
-      {commentEditable ? <Button>저장</Button> : <></>}
-    </Style.Container>
+    <>
+      {!!error && <AlertModal content={error} clickCloseButton={handleCloseModal} />}
+      <Style.Container onSubmit={handleFormSubmit}>
+        <Style.Info>
+          <Style.NicknameWrapper>{comment.nickname}</Style.NicknameWrapper>
+          <Style.DateWrapper>{dateToString(comment.createdAt)}</Style.DateWrapper>
+          {editableAuthority && !commentEditable ? (
+            <>
+              <Button formButton={false} onClick={toggleCommentEditable}>
+                수정
+              </Button>
+              <Button formButton={false} theme="secondary" onClick={handleClickDeleteButton}>
+                삭제
+              </Button>
+            </>
+          ) : (
+            <></>
+          )}
+        </Style.Info>
+        <EditableText
+          isEditable={commentEditable}
+          textType="paragraph"
+          paragraphProps={{ size: 'medium', content: '' }}
+          textareaProps={textareaProps}
+        />
+        {commentEditable ? <Button>저장</Button> : <></>}
+      </Style.Container>
+    </>
   );
 }
 
--- src/validator/commentValidator.ts
+++ src/validator/commentValidator.ts
@@ -1,4 +1,5 @@
 export interface CommentValidatorType {
+  commentId?: string;
   comment: string;
 }
 
